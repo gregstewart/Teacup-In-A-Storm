e8557a4cc4dd26bc75faba1e162a5ce9e880f52a
Greg Stewart <gregs@tcias.co.uk>
2011-01-01 18:40:46 +0000

    Done with static pages

--- rails-www/app/controllers/pages_controller.rb
+++ rails-www/app/controllers/pages_controller.rb
@@ -1,11 +1,14 @@
 class PagesController < ApplicationController
   def home
+    @title = "home"
   end
 
   def portfolio
+    @title = "portfolio"
   end
 
   def cv
+    @title = "cv"
   end
 
 end
--- rails-www/spec/controllers/pages_controller_spec.rb
+++ rails-www/spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,18 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title",
+                        :content => "TCIAS | home")
     end
   end
 
@@ -14,12 +21,24 @@ describe PagesController do
       get 'portfolio'
       response.should be_success
     end
+
+    it "should have the right title" do
+      get 'portfolio'
+      response.should have_selector("title",
+                        :content => "TCIAS | portfolio")
+    end
   end
 
   describe "GET 'cv'" do
     it "should be successful" do
       get 'cv'
       response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'cv'
+      response.should have_selector("title",
+                        :content => "TCIAS | cv")
     end
   end
 
