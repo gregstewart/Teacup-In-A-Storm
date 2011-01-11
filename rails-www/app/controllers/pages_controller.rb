e98fc98eaa66169dd6bee8989bfa9703e7a4906b
Greg Stewart <gregs@tcias.co.uk>
2011-01-11 13:38:18 +0000

    added favicon to code base and .gitignore

--- rails-www/app/controllers/pages_controller.rb
+++ rails-www/app/controllers/pages_controller.rb
@@ -1,14 +1,23 @@
 class PagesController < ApplicationController
   def home
     @title = "home"
+    respond_to do |format|
+      format.html
+    end
   end
 
   def portfolio
     @title = "portfolio"
+    respond_to do |format|
+      format.html
+    end
   end
 
   def cv
     @title = "cv"
+    respond_to do |format|
+      format.html
+    end
   end
 
 end
