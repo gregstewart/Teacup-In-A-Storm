2b62ddecef237317a51c6285a47fce87889e43e5
Greg Stewart <gregs@tcias.co.uk>
2011-01-24 13:30:07 +0000

    now only rails version of the site - getting ready for deployment to Heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsWww::Application.config.secret_token = '39a5bc32a711cf02cd62b1170736ad8f2d760b0f19ec924541cd85cd05ff15dfca7f07eb211d4bf367f550a513e5cbfdacdaaa3319043c9d1be4daf52b3a73b6'
