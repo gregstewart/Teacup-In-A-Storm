1ffea8f2910b5bd726380198a428897a5ba41fa4
Greg Stewart <greg@tcias.co.uk>
2013-03-10 19:35:24 +0000

    Finished with the API calls for now

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,28 +1,33 @@
 class PagesController < ApplicationController
+
+  caches_page :home
+
   def home
     @title = "home"
 
-    @wordpress_feed = Feed.new("http://gregs.tcias.co.uk/feed/")
-    @wordpress_entries = @wordpress_feed.get_last_five
+    #Rails.cache.fetch("home_page", :expires_in => 12.hours) do
+      @wordpress_feed = Feed.new("http://gregs.tcias.co.uk/feed/")
+      @wordpress_entries = @wordpress_feed.get_last_five
 
-    @delicious_feed = Feed.new("http://feeds.delicious.com/v2/rss/wildcard1999")
-    @delicious_entries = @delicious_feed.get_last_five
+      @delicious_feed = Feed.new("http://feeds.delicious.com/v2/rss/wildcard1999")
+      @delicious_entries = @delicious_feed.get_last_five
 
-    @instagram_feed = Instagram.user_recent_media(178138400)
-    @instagram_entries = @instagram_feed[0..4]
+      @instagram_feed = Instagram.user_recent_media(178138400)
+      @instagram_entries = @instagram_feed[0..5]
 
-    @github_feed = Octokit.user_events("gregstewart")
-    @github_entries = @github_feed[0..4]
+      @github_feed = Octokit.user_events("gregstewart")
+      @github_entries = @github_feed[0..4]
 
-    @twitter_feed = Twitter.user_timeline("_greg_stewart_")
-    @twitter_entries = @twitter_feed[0..4]
+      @twitter_feed = Twitter.user_timeline("_greg_stewart_")
+      @twitter_entries = @twitter_feed[0..4]
 
-    @vimeo_feed = Vimeo::Simple::User.videos("user2724002")
-    @vimeo_entries = @vimeo_feed[0..4]
+      @vimeo_feed = Vimeo::Simple::User.videos("user2724002")
+      @vimeo_entries = @vimeo_feed[0..1]
 
-    respond_to do |format|
-      format.html
-    end
+      respond_to do |format|
+        format.html
+      end
+    #end
   end
 
   def portfolio
