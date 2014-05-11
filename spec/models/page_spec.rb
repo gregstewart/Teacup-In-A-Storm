1b3822745b7264654cd42e35399647e4d091f0b6
Greg Stewart <gregs@tcias.co.uk>
2014-05-11 19:38:03 +0100

    Fixed failing test after adding location object to page model

--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -5,9 +5,9 @@ describe 'Page' do
   it 'should sort a collection of items by date in descending order' do
     page = Page.new
     items = page.items
-    items.push(page.set_page_item('type', DateTime.new(2013,7,12), 'some text 1', 'some url', 'some thumbnail'))
-    items.push(page.set_page_item('type', DateTime.new(2013,8,23), 'some text 2', 'some url', 'some thumbnail'))
-    items.push(page.set_page_item('type', DateTime.new(2013,8,01), 'some text 3', 'some url', 'some thumbnail'))
+    items.push(page.set_page_item('type', DateTime.new(2013,7,12), 'some text 1', 'some url', 'some thumbnail', nil))
+    items.push(page.set_page_item('type', DateTime.new(2013,8,23), 'some text 2', 'some url', 'some thumbnail', nil))
+    items.push(page.set_page_item('type', DateTime.new(2013,8,01), 'some text 3', 'some url', 'some thumbnail', nil))
 
     page.sort_by_date
     #To change this template use File | Settings | File Templates.
