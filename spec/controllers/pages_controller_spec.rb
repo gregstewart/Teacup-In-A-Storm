decfeab6a23407f424f751089a44e720efb0da71
Greg Stewart <gregs@tcias.co.uk>
2015-01-11 15:38:19 +0000

    Fixing up controller tests (min/max content checks) and code formatting tweaked

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -34,55 +34,58 @@ describe PagesController do
       end
 
       it "should have the right title" do
-        page.should have_selector("title",
-                                      :content => "TCIAS | Home")
+        page.should have_selector("title", :content => "TCIAS | Home")
       end
 
       it "should have only the tcias breadcrumb" do
-        page.should_not have_selector("li.breadcrumb",
-                                          :text => "portfolio")
-        page.should_not have_selector("li.breadcrumb",
-                                          :text => "cv")
+        page.should_not have_selector("li.breadcrumb", :text => "portfolio")
+        page.should_not have_selector("li.breadcrumb", :text => "cv")
       end
 
       it "should have google analytics" do
-        page.should have_selector("script",
-                                      :content => "_gaq")
+        page.should have_selector("script", :content => "_gaq")
       end
 
       it "should have 5 delicious links" do
         matching = page.all('li.delicious')
-        matching.size.should be(5)
+        matching.should have_at_least(1).items
+        matching.should have_at_most(5).items
       end
 
       it "should have 10 blog post links" do
         matching = page.all('li.wordpress')
-        matching.size.should be(10)
+        matching.should have_at_least(1).items
+        matching.should have_at_least(10).items
       end
 
       it "should have 2 vimeo links" do
         matching = page.all('li.vimeo')
-        matching.size.should be(2)
+        matching.should have_at_least(1).items
+        matching.should have_at_least(2).items
       end
 
       it "should have 5 twitter links" do
         matching = page.all('li.twitter')
-        matching.size.should be(5)
+        matching.should have_at_least(1).items
+        matching.should have_at_least(5).items
       end
 
       it "should have 6 instagram links" do
         matching = page.all('li.instagram')
-        matching.size.should be(6)
+        matching.should have_at_least(1).items
+        matching.should have_at_least(6).items
       end
 
       it "should have 5 github links" do
         matching = page.all('li.github')
-        matching.size.should be(5)
+        matching.should have_at_least(1).items
+        matching.should have_at_least(5).items
       end
 
       it "should have 10 foursquare links" do
         matching = page.all('li.foursquare')
-        matching.size.should be(10)
+        matching.should have_at_least(1).items
+        matching.should have_at_least(10).items
       end
     end
 
@@ -96,15 +99,13 @@ describe PagesController do
 
     it "should have the right title" do
       visit portfolio_path
-      page.should have_selector("title",
-                                    :content => "TCIAS | Portfolio")
+      page.should have_selector("title", :content => "TCIAS | Portfolio")
     end
 
     it "should have the right breadcrumb" do
       visit portfolio_path
 
-      page.should have_selector("li.breadcrumb",
-                                    :text => "portfolio")
+      page.should have_selector("li.breadcrumb", :text => "portfolio")
     end
 
   end
@@ -118,15 +119,13 @@ describe PagesController do
     it "should have the right title" do
       visit cv_path
 
-      page.should have_selector("title",
-                                    :content => "TCIAS | Curriculum Vitae")
+      page.should have_selector("title", :content => "TCIAS | Curriculum Vitae")
     end
 
     it "should have the right breadcrumb" do
       visit cv_path
 
-      page.should have_selector("li.breadcrumb",
-                                    :text => "cv")
+      page.should have_selector("li.breadcrumb", :text => "cv")
     end
   end
 
