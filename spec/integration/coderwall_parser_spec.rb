da7f4341e9cebda8122dbe4af92335425b781e35
Greg Stewart <gregs@tcias.co.uk>
2015-04-23 23:09:53 +0100

    Fixed strange formatting issue with images on timeline view and tweaked coderwall render

---
+++ spec/integration/coderwall_parser_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe 'Foursquare Parser' do
+
+  it 'should return 10 items from Foursquare' do
+    foursquare_client = Foursquare.new
+
+    foursquare_items = foursquare_client.get_last_user_events(10)
+
+    foursquare_items.size.should == 10
+  end
+end
