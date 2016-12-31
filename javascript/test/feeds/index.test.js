619e66e7048d8a25b659b461104dfa5f7ed25cfb
Greg Stewart <gregs@tcias.co.uk>
2016-12-31 18:47:05 +0100

    Added delicious feed

--- javascript/test/feeds/index.test.js
+++ javascript/test/feeds/index.test.js
@@ -5,10 +5,15 @@ import client from '../../feeds';
 
 describe('Feeds', () => {
   before(() => {
-    const feed = fs.readFileSync('./test/fixtures/atom.xml', 'utf-8');
+    const blogFeed = fs.readFileSync('./test/fixtures/atom.xml', 'utf-8');
+    const deliciousFeed = fs.readFileSync('./test/fixtures/delicious.txt', 'utf-8');
     nock('https://www.tcias.co.uk')
       .get('/blog/atom.xml')
-      .reply(200, feed);
+      .reply(200, blogFeed);
+
+    nock('http://feeds.del.icio.us')
+      .get('/v2/rss/wildcard1999')
+      .reply(200, deliciousFeed);
   });
   describe('Blog', () => {
     it('throws an exception if the url is missing', (done) => {
@@ -31,6 +36,24 @@ describe('Feeds', () => {
           link: 'www.tcias.co.uk/blog/2015/08/07/your-organisation-should-adopt-an-open-source-model/',
           title: 'Your Organisation Should Adopt an Open Source Model',
           updated: '2015-08-07T10:25:04+00:00' });
+        done();
+      })
+      .catch((error) => {
+        expect(error).to.equal(undefined);
+        done();
+      });
+    });
+  });
+
+  describe('Delicious', () => {
+    it('fetches a feed', (done) => {
+      client.get('http://feeds.del.icio.us/v2/rss/wildcard1999')
+      .then((response) => {
+        expect(response[0]).to.deep.equal({ id: 'https://shop.icio.us/sales/the-limited-edition-black-hawk-drone-hd-camera?utm_source=del.icio.us&utm_medium=referral&utm_campaign=the-limited-edition-black-hawk-drone-hd-camera',
+          link: 'https://shop.icio.us/sales/the-limited-edition-black-hawk-drone-hd-camera?utm_source=del.icio.us&utm_medium=referral&utm_campaign=the-limited-edition-black-hawk-drone-hd-camera',
+          summary: 'Our #1 Best-Selling Drone--Meet the Dark Night of the Sky!',
+          title: 'Sponsored: 64% off Code Black Drone with HD Camera',
+          updated: 'Sat, 31 Dec 2016 17:43:28 -0000' });
         done();
       })
       .catch((error) => {
