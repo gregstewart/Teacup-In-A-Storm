7798ff03732199dc67e76d4a50b542db28904c60
Greg Stewart <gregs@tcias.co.uk>
2017-03-25 09:59:22 +0000

    Extract data

--- ui/src/ui/core.cljs
+++ ui/src/ui/core.cljs
@@ -36,47 +36,47 @@
       [:div.eight.columns
         (contact-panel-renderer @app-state)]
       [:div.four.columns.omega
-        (box "boxee" "linked-in" (masthead-component ["L" "http://www.linkedin.com/in/gregstewart" 3 "Click to view my LinkedIn profile" "icon-linkedin-sign"]))]]
+        (box "boxee" :linked-in (masthead-component (get-in @app-state [:linked-in :details])))]]
     [:div.row.clearfix
       [:div.four.columns.alpha.isotope
         (box "boxee"
             :delicious
-            (masthead-component ["D" "https://delicious.com/wildcard1999" 5 "Click to view my Delicious profile" "icon-delicious"])
+            (masthead-component (get-in @app-state [:delicious :details]))
             [feed-renderer :delicious "5 most recent bookmarks" (get-in @app-state [:delicious :list-items])])]
       [:div.six.columns.isotope
         (box "boxee"
             :wordpress
-            (masthead-component ["W" "https://www.tcias.co.uk/blog" 6 "Click to view my blog" "icon-wordpress"])
+            (masthead-component (get-in @app-state [:wordpress :details]))
             [feed-renderer :wordpress "10 most recent posts" (get-in @app-state [:wordpress :list-items])])]
       [:div.six.columns.omega.isotope
         (box "boxee"
             :vimeo
-            (masthead-component ["V" "https://vimeo.com/user2724002/videos" 7 "Click to view my Vimeo profile" "icon-vimeo-sign"])
+            (masthead-component (get-in @app-state [:vimeo :details]))
             [feed-renderer :vimeo "Recent videos" (get-in @app-state [:vimeo :list-items]) "images"])]]
     [:div.row.clearfix
       [:div.six.columns.alpha.isotope
         (box "boxee stacked"
             :twitter
-            (masthead-component ["T" "https://twitter.com/_greg_stewart_" 8 "Click to view my Twitter profile" "icon-twitter"])
+            (masthead-component (get-in @app-state [:twitter :details]))
             [feed-renderer :twitter "Recent tweets" (get-in @app-state [:twitter :list-items])])
         (box "boxee stacked"
             :foursquare
-            (masthead-component ["F" "https://foursquare.com/user/13278548" 9 "Click to view my Foursquare profile" "icon-foursquare"])
+            (masthead-component (get-in @app-state [:foursquare :details]))
             [feed-renderer :foursquare "Recent checkins" (get-in @app-state [:foursquare :list-items]) "map-list"])]
       [:div.six.columns.isotope
         (box "boxee stacked"
             :instagram
-            (masthead-component ["I" "http://instagram.com/_greg_stewart_" 10 "Click to view my Instagram profile" "icon-instagram"])
+            (masthead-component (get-in @app-state [:instagram :details]))
             [feed-renderer :instagram "6 most recent images" (get-in @app-state [:instagram :list-items]) "images"])
-        (box "boxee stacked" "last-fm" (masthead-component ["L" "http://www.last.fm/user/greg_stewart" 11 "Click to view my Last.fm profile" "icon-lastfm-sign"]))
-        (box "boxee stacked" "flickr" (masthead-component ["R" "http://www.flickr.com/photos/greg_and_jodie/" 12 "Click to view my Flickr profile" "icon-flickr-sign"]))]
+        (box "boxee stacked" :last-fm (masthead-component (get-in @app-state [:last-fm :details])))
+        (box "boxee stacked" :flickr (masthead-component (get-in @app-state [:flickr :details])))]
       [:div.four.columns.omega.isotope
         (box "boxee stacked"
             :github
-            (masthead-component ["H" "https://github.com/gregstewart" 13 "Click to view my GitHub profile" "icon-github"])
+            (masthead-component (get-in @app-state [:github :details]))
             [feed-renderer :github "5 most recent events" (get-in @app-state [:github :list-items])])
-        (box "boxee stacked" "stackover-flow" (masthead-component ["O" "http://stackoverflow.com/users/197825/greg-stewart" 14 "Click to view my Stackoverflow profile" "icon-stack-overflow"]))
-        (box "boxee stacked" "google-plus" (masthead-component ["G" "https://plus.google.com/110643069434566075441" 15 "Click to view my Google+ profile" "icon-google-plus"]))]]])
+        (box "boxee stacked" :stackover-flow (masthead-component (get-in @app-state [:stackover-flow :details])))
+        (box "boxee stacked" :google-plus (masthead-component (get-in @app-state [:google-plus :details])))]]])
 
 (reagent/render-component [layout];
                           (. js/document (getElementById "app")))
--- ui/src/ui/data.cljs
+++ ui/src/ui/data.cljs
@@ -10,7 +10,10 @@
             :work-number "+44 208 2869246"
             :mobile-number "+44 7891 032239"
             :skype-number "skype:greg.stewart.work?call"
+            :linked-in {
+                        :details ["L" "http://www.linkedin.com/in/gregstewart" 3 "Click to view my LinkedIn profile" "icon-linkedin-sign"]}
             :delicious {
+                        :details ["D" "https://delicious.com/wildcard1999" 5 "Click to view my Delicious profile" "icon-delicious"]
                         :list-items {
                                       :type "delicious"
                                       :items [{:link "https://shop.icio.us/sales/the-limited-edition-black-hawk-drone-hd-camera?utm_source=del.icio.us&utm_medium=referral&utm_campaign=the-limited-edition-black-hawk-drone-hd-camera"
@@ -24,6 +27,7 @@
                                               {:link "http://del.icio.us/url/5e559a3d88ab8fa5079b59161f802609#wildcard1999"
                                                 :value "Clojurescript & Node on AWS Lambda – Nervous Systems"}]}}
             :wordpress {
+                        :details ["W" "https://www.tcias.co.uk/blog" 6 "Click to view my blog" "icon-wordpress"]
                         :list-items {
                                       :type "wordpress"
                                       :items [{:link "https://www.tcias.co.uk/blog/2015/08/07/your-organisation-should-adopt-an-open-source-model"
@@ -31,6 +35,7 @@
                                               {:link "https://www.tcias.co.uk/blog/2015/06/28/why-use-node-dot-js"
                                                 :value "Why Use Node.js"}]}}
             :vimeo {
+                    :details ["V" "https://vimeo.com/user2724002/videos" 7 "Click to view my Vimeo profile" "icon-vimeo-sign"]
                     :list-items {
                                   :type "vimeo"
                                   :items [{:link "https://vimeo.com/93167466"
@@ -40,6 +45,7 @@
                                             :value "Portland vs Houston - opening"
                                             :image "https://i.vimeocdn.com/video/473143582_640.jpg"}]}}
             :twitter {
+                      :details ["T" "https://twitter.com/_greg_stewart_" 8 "Click to view my Twitter profile" "icon-twitter"]
                       :list-items {
                                     :type "twitter"
                                     :items [{:link "https://twitter.com/_greg_stewart_/status/761244885571821568"
@@ -49,6 +55,7 @@
                                               :value "RT @redbadgerteam: Interested in joining our BRILLIANT team? We are hiring! Check out our vacancies here- https://t.co/OXah9qF7xe #jobs htt…"
                                               :date " - 2016/08/04 @ 16:49"}]}}
             :foursquare {
+                          :details ["F" "https://foursquare.com/user/13278548" 9 "Click to view my Foursquare profile" "icon-foursquare"]
                           :list-items {
                                         :type "foursquare"
                                         :items [{:lat "51.50746113725417"
@@ -60,13 +67,23 @@
                                                   :value "Itsu"
                                                   :date " - 2016/08/19 @ 12:08"}]}}
             :instagram {
+                        :details ["I" "http://instagram.com/_greg_stewart_" 10 "Click to view my Instagram profile" "icon-instagram"]
                         :list-items {
                                       :type "instagram"
                                       :items [{:link "https://www.instagram.com/p/BJMrkkDhgFE/"
                                                 :image "https://scontent.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/13703165_486634748282966_1570166956_n.jpg?ig_cache_key=MTMxODYyMDQxOTUyNzY3MjEzMg%3D%3D.2"}
                                               {:link "https://www.instagram.com/p/BJDBPllh0lQ/"
                                                 :image "https://scontent.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/13687050_1212816692061982_1717695137_n.jpg?ig_cache_key=MTMxNTkwMDk4NTMxOTA0OTU1Mg%3D%3D.2"}]}}
+            :last-fm {
+                      :details ["L" "http://www.last.fm/user/greg_stewart" 11 "Click to view my Last.fm profile" "icon-lastfm-sign"]}
+            :flickr {
+                      :details ["R" "http://www.flickr.com/photos/greg_and_jodie/" 12 "Click to view my Flickr profile" "icon-flickr-sign"]}
+            :stackover-flow {
+                              :details ["O" "http://stackoverflow.com/users/197825/greg-stewart" 14 "Click to view my Stackoverflow profile" "icon-stack-overflow"]}
+            :google-plus {
+                          :details ["G" "https://plus.google.com/110643069434566075441" 15 "Click to view my Google+ profile" "icon-google-plus"]}
             :github {
+                      :details ["H" "https://github.com/gregstewart" 13 "Click to view my GitHub profile" "icon-github"]
                       :list-items {
                                     :type "github"
                                     :items [{:link "https://api.github.com/repos/gregstewart/hearthstone-tracker"
