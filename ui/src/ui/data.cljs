0e71ee8c87aaed0158cb92ec08c989ded9ee8446
Greg Stewart <gregs@tcias.co.uk>
2017-04-23 18:48:19 +0100

    Merge personal details with json structure

--- ui/src/ui/data.cljs
+++ ui/src/ui/data.cljs
@@ -3,7 +3,18 @@
 
 (def r (transit/reader :json))
 
+(def data { :given-name "Greg "
+            :family-name "Stewart"
+            :title "Chief Technology Officer"
+            :home-email "gregs@tcias.co.uk"
+            :home-mailto "mailto:gregs+personal-website@tcias.co.uk?subject=Hello from the web"
+            :work-email "greg.stewart@red-badger.com"
+            :work-mailto "mailto:greg.stewart+personal-website@red-badger.com?subject=Hello from the web"
+            :work-number "+44 208 2869246"
+            :mobile-number "+44 7891 032239"
+            :skype-number "skype:greg.stewart.work?call"})
+
 (defn get-data
   "returns the data"
   [json]
-  (transit/read r json))
+  (conj (transit/read r json) data))
