c84b8f7df224103135787f958238b64d9eefe5f7
Greg Stewart <gregs@tcias.co.uk>
2018-04-04 12:12:53 +0100

    Add expiration header

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,5 +2,6 @@
 
 aws s3 sync ui/resources/public s3://tcias.co.uk/ --acl public-read  \
 --cache-control 'max-age=86400'  \
+--expires "$(date -d '+6 months' --utc +'%Y-%m-%dT%H:%M:%SZ')"
 --region eu-west-1 \
 --include="*"  \
